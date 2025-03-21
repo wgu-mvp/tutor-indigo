28d22a8c5983aa8c45dbc1ff55807b2b204d7280
Cory Hammon <cory.hammon@wgu.edu>
2025-03-21 17:22:40 -0600

    Restyled home page

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -29,8 +29,6 @@ config: t.Dict[str, t.Dict[str, t.Any]] = {
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[]
         "FOOTER_NAV_LINKS": [
             {"title": "About Us", "url": "/about"},
-            {"title": "Blog", "url": "/blog"},
-            {"title": "Donate", "url": "/donate"},
             {"title": "Terms of Service", "url": "/tos"},
             {"title": "Privacy Policy", "url": "/privacy"},
             {"title": "Help", "url": "/help"},
